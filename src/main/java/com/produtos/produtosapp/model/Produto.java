e6bf3c376da70679debe0cd46f1bcf01bc3b00f6
joncleber <37044410+joncleber@users.noreply.github.com>
2018-05-08 19:25:34 -0300

    Relação entre as entidades Produto e Revisao

--- src/main/java/com/produtos/produtosapp/model/Produto.java
+++ src/main/java/com/produtos/produtosapp/model/Produto.java
@@ -1,11 +1,13 @@
 package com.produtos.produtosapp.model;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -14,6 +16,10 @@ public class Produto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long produtoId;
+	
+	@OneToMany
+	List<Revisao> revisao;
+	
 	
 	@NotNull
 	private String nome;
@@ -59,6 +65,14 @@ public class Produto {
 	private Date dataModificacao;
 	
 	
+
+	public List<Revisao> getRevisao() {
+		return revisao;
+	}
+
+	public void setRevisao(List<Revisao> revisao) {
+		this.revisao = revisao;
+	}
 
 	public Produto() {
 		super();
---
+++ src/main/java/com/produtos/produtosapp/model/Revisao.java
@@ -0,0 +1,90 @@
+package com.produtos.produtosapp.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Revisao {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long revisaoId;
+	
+	private Produto produto;
+	
+	@NotNull
+	private String nomeRevisao;
+	@NotNull
+	private Date dataRevisao;
+	@NotNull
+	private String enderecoEmail;
+	
+	private String comentarios;
+	@NotNull
+	private Date dataModificacao;
+	
+	
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public Revisao() {
+		super();
+		dataModificacao = new Date();
+	}
+	public Revisao(long revisaoId, String nomeRevisao, Date dataRevisao, String enderecoEmail, String comentarios,
+			Date dataModificacao) {
+		super();
+		this.revisaoId = revisaoId;
+		this.nomeRevisao = nomeRevisao;
+		this.dataRevisao = dataRevisao;
+		this.enderecoEmail = enderecoEmail;
+		this.comentarios = comentarios;
+		this.dataModificacao = dataModificacao;
+	}
+	public long getRevisaoId() {
+		return revisaoId;
+	}
+	public void setRevisaoId(long revisaoId) {
+		this.revisaoId = revisaoId;
+	}
+	public String getNomeRevisao() {
+		return nomeRevisao;
+	}
+	public void setNomeRevisao(String nomeRevisao) {
+		this.nomeRevisao = nomeRevisao;
+	}
+	public Date getDataRevisao() {
+		return dataRevisao;
+	}
+	public void setDataRevisao(Date dataRevisao) {
+		this.dataRevisao = dataRevisao;
+	}
+	public String getEnderecoEmail() {
+		return enderecoEmail;
+	}
+	public void setEnderecoEmail(String enderecoEmail) {
+		this.enderecoEmail = enderecoEmail;
+	}
+	public String getComentarios() {
+		return comentarios;
+	}
+	public void setComentarios(String comentarios) {
+		this.comentarios = comentarios;
+	}
+	public Date getDataModificacao() {
+		return dataModificacao;
+	}
+	public void setDataModificacao(Date dataModificacao) {
+		this.dataModificacao = dataModificacao;
+	}
+
+}
